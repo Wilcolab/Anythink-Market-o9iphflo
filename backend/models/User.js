602b23a480921e5599eb7dcc4e2d30e45620bdb2
Biruk Kebede <61163684+birukkebede11@users.noreply.github.com>
2023-01-10 17:53:29 -0500

    added a method

--- backend/models/User.js
+++ backend/models/User.js
@@ -43,92 +43,99 @@ var UserSchema = new mongoose.Schema(
 
 UserSchema.plugin(uniqueValidator, { message: "is already taken." });
 
-UserSchema.methods.validPassword = function(password) {
-  var hash = crypto
-    .pbkdf2Sync(password, this.salt, 10000, 512, "sha512")
-    .toString("hex");
-  return this.hash === hash;
+UserSchema.methods.validPassword = function (password) {
+	var hash = crypto
+		.pbkdf2Sync(password, this.salt, 10000, 512, "sha512")
+		.toString("hex");
+	return this.hash === hash;
 };
 
-UserSchema.methods.setPassword = function(password) {
-  this.salt = crypto.randomBytes(16).toString("hex");
-  this.hash = crypto
-    .pbkdf2Sync(password, this.salt, 10000, 512, "sha512")
-    .toString("hex");
+UserSchema.methods.setPassword = function (password) {
+	this.salt = crypto.randomBytes(16).toString("hex");
+	this.hash = crypto
+		.pbkdf2Sync(password, this.salt, 10000, 512, "sha512")
+		.toString("hex");
 };
 
-UserSchema.methods.generateJWT = function() {
-  var today = new Date();
-  var exp = new Date(today);
-  exp.setDate(today.getDate() + 60);
+UserSchema.methods.generateJWT = function () {
+	var today = new Date();
+	var exp = new Date(today);
+	exp.setDate(today.getDate() + 60);
 
-  return jwt.sign(
-    {
-      id: this._id,
-      username: this.username,
-      exp: parseInt(exp.getTime() / 1000)
-    },
-    secret
-  );
+	return jwt.sign(
+		{
+			id: this._id,
+			username: this.username,
+			exp: parseInt(exp.getTime() / 1000),
+		},
+		secret
+	);
 };
 
-UserSchema.methods.toAuthJSON = function() {
-  return {
-    username: this.username,
-    email: this.email,
-    token: this.generateJWT(),
-    bio: this.bio,
-    image: this.image,
-    role: this.role
-  };
+UserSchema.methods.toAuthJSON = function () {
+	return {
+		username: this.username,
+		email: this.email,
+		token: this.generateJWT(),
+		bio: this.bio,
+		image: this.image,
+		role: this.role,
+	};
 };
 
-UserSchema.methods.toProfileJSONFor = function(user) {
-  return {
-    username: this.username,
-    bio: this.bio,
-    image:
-      this.image || "https://static.productionready.io/images/smiley-cyrus.jpg",
-    following: user ? user.isFollowing(this._id) : false
-  };
+UserSchema.methods.toProfileJSONFor = function (user) {
+	return {
+		username: this.username,
+		bio: this.bio,
+		image:
+			this.image || "https://static.productionready.io/images/smiley-cyrus.jpg",
+		following: user ? user.isFollowing(this._id) : false,
+	};
 };
 
-UserSchema.methods.favorite = function(id) {
-  if (this.favorites.indexOf(id) === -1) {
-    this.favorites = this.favorites.concat([id]);
-  }
+UserSchema.methods.favorite = function (id) {
+	if (this.favorites.indexOf(id) === -1) {
+		this.favorites = this.favorites.concat([id]);
+	}
 
-  return this.save();
+	return this.save();
 };
 
-UserSchema.methods.unfavorite = function(id) {
-  this.favorites.remove(id);
-  return this.save();
+UserSchema.methods.unfavorite = function (id) {
+	this.favorites.remove(id);
+	return this.save();
 };
 
-UserSchema.methods.isFavorite = function(id) {
-  return this.favorites.some(function(favoriteId) {
-    return favoriteId.toString() === id.toString();
-  });
+UserSchema.methods.isFavorite = function (id) {
+	return this.favorites.some(function (favoriteId) {
+		return favoriteId.toString() === id.toString();
+	});
 };
 
-UserSchema.methods.follow = function(id) {
-  if (this.following.indexOf(id) === -1) {
-    this.following = this.following.concat([id]);
-  }
+UserSchema.methods.follow = function (id) {
+	if (this.following.indexOf(id) === -1) {
+		this.following = this.following.concat([id]);
+	}
 
-  return this.save();
+	return this.save();
 };
 
-UserSchema.methods.unfollow = function(id) {
-  this.following.remove(id);
-  return this.save();
+UserSchema.methods.unfollow = function (id) {
+	this.following.remove(id);
+	return this.save();
 };
 
-UserSchema.methods.isFollowing = function(id) {
-  return this.following.some(function(followId) {
-    return followId.toString() === id.toString();
-  });
+UserSchema.methods.isFollowing = function (id) {
+	return this.following.some(function (followId) {
+		return followId.toString() === id.toString();
+	});
+};
+
+UserSchema.methods.verify = function (id) {
+	if (this.isVerified) {
+		this.isVerified = true;
+	}
+	return this.save();
 };
 
 mongoose.model("User", UserSchema);
