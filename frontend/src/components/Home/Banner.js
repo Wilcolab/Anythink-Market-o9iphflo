da2b5b65eb5d9592cd531190ca4b02b5dc3aa617
WilcoApp <hello@wilco.gg>
2023-01-31 06:26:42 +0000

    To the Cloud and Beyond

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,26 +1,19 @@
 import React from "react";
 import logo from "../../imgs/logo.png";
-import styled from "styled-components";
-
-const Div = styled.div`
-	background-color: ${(props) => props.theme.dark};
-`;
 
 const Banner = () => {
-	return (
-		<Div>
-			<div className="banner text-white">
-				<div className="container p-4 text-center">
-					<img src={logo} alt="banner" />
-					<div>
-						<span>A place to </span>
-						<span id="get-part">get</span>
-						<span> the cool stuff.</span>
-					</div>
-				</div>
-			</div>
-		</Div>
-	);
+  return (
+    <div className="banner text-white">
+      <div className="container p-4 text-center">
+        <img src={logo} alt="banner" />
+        <div>
+          <span>A place to </span>
+          <span id="get-part">get</span>
+          <span> the cool stuff.</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,23 +7,16 @@ import { store, history } from "./store";
 import { Route, Switch } from "react-router-dom";
 import { ConnectedRouter } from "react-router-redux";
 
-import { ThemeProvider } from "styled-components";
-import theme from "./styles/theme";
-
 import App from "./components/App";
-import GlobalStyle from "./styles/GlobalStyle";
 
 ReactDOM.render(
-	<Provider store={store}>
-		<ConnectedRouter history={history}>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-				<Switch>
-					<Route path="/" component={App} />
-				</Switch>
-			</ThemeProvider>
-		</ConnectedRouter>
-	</Provider>,
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <Switch>
+        <Route path="/" component={App} />
+      </Switch>
+    </ConnectedRouter>
+  </Provider>,
 
-	document.getElementById("root")
+  document.getElementById("root")
 );
