d464a69a5fb7a3adc6d21fd0c3db9d5588256687
Biruk Kebede <61163684+birukkebede11@users.noreply.github.com>
2023-02-26 01:04:53 -0500

    Redirect if not authenticated

--- frontend/src/components/PrivateRoute.js
+++ frontend/src/components/PrivateRoute.js
@@ -1,12 +1,21 @@
 import React from 'react'
-import {  Route } from 'react-router';
+import { Redirect, Route } from 'react-router';
 
 function PrivateRoute({ children, auth, ...rest }) {
 	return (
 		<Route
 			{...rest}
 			render={({ location }) =>
-				auth ? <div>Protected Content</div> : null
+				auth ? (
+					children
+				) : (
+					<Redirect
+						to={{
+							pathname: "/login",
+							state: { from: location },
+						}}
+					/>
+				)
 			}
 		/>
 	);
