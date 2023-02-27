a4bf7ad3b2dae6636ddea00e5eab7b91a54a6831
Biruk Kebede <61163684+birukkebede11@users.noreply.github.com>
2023-02-27 17:33:13 -0500

    Added a 404 page

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -14,6 +14,7 @@ import Register from "./Register";
 import Settings from "./Settings";
 import { store } from "../store";
 import { push } from "react-router-redux";
+import NotFound from "./NotFound";
 
 const mapStateToProps = (state) => {
   return {
@@ -66,6 +67,7 @@ class App extends React.Component {
             <Route path="/settings" component={Settings} />
             <Route path="/@:username/favorites" component={ProfileFavorites} />
             <Route path="/@:username" component={Profile} />
+            <Route path="/" component={NotFound} />
           </Switch>
         </div>
       );
