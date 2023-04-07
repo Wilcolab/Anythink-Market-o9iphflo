4a1279c87ed2691ba27e69868078e366027ae91e
Biruk Kebede <61163684+birukkebede11@users.noreply.github.com>
2023-04-07 15:17:42 -0400

    fix: script

--- backend/scripts/backup.sh
+++ backend/scripts/backup.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Get the log file path from the command line argument
 log_file_path=$1
 
@@ -8,7 +10,7 @@ if [ ! -f "$log_file_path" ]; then
 fi
 
 # Create the backup archive
-backup_filename="../../backup.tar.gz"
+backup_filename="./backup.tar.gz"
 tar -czf $backup_filename $log_file_path
 
 echo "$log_file_path backed up to $backup_filename."
